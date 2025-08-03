cfd7ca5f36cd766d9bb351b28b8d05263e0417a1
mtmozart <mmsnborges@gmail.com>
2025-08-03 14:48:00 -0300

    feat: add the backend url's

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from "rxjs";
   providedIn: "root",
 })
 export class ApiService {
-  private readonly baseUrl = "http://localhost:8080";
+  private readonly baseUrl = "https://ballon-lbxn.onrender.com";
 
   constructor(private http: HttpClient) {}
 
